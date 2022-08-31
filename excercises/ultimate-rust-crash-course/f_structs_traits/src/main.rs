2ba8efa4bfa26f292083484b6117764cedbb2214
Jens <jens.kristian@kodebyraaet.no>
2022-08-31 16:18:41 +0200

    exercise: added solution for urcc/f_structs

--- excercises/ultimate-rust-crash-course/f_structs_traits/src/main.rs
+++ excercises/ultimate-rust-crash-course/f_structs_traits/src/main.rs
@@ -6,19 +6,36 @@
 //
 //  trait Bite...
 
+// SOLUTION:
+trait Bite {
+    fn bite(self: &mut Self);
+}
+
 
 // 2. Now create a struct named Grapes with a field that tracks how many grapes are left.  If you
 // need a hint, look at how it was done for Carrot at the bottom of this file (you should probably
 // use a different field, though).
 //
-// #[derive(Debug)] // include this line right before your struct definition
+#[derive(Debug)] // include this line right before your struct definition
 // struct Grapes...
 
+// SOLUTION:
+struct Grapes {
+    quantity: i32,
+}
 
 // 3. Implement Bite for Grapes.  When you bite a Grapes, subtract 1 from how many grapes are left.
 // If you need a hint, look at how it was done for Carrot at the bottom of this file.
 //
 // impl Bite for...
+
+// SOLUTION:
+impl Bite for Grapes {
+    fn bite(self: &mut Self) {
+        self.quantity -= 1;
+    }
+}
+
 
 
 fn main() {
@@ -34,6 +51,11 @@ fn main() {
     //grapes.bite();
     //println!("Eat a grape: {:?}", grapes);
 
+    // SOLUTION:
+    let mut grapes = Grapes { quantity: 100 };
+    grapes.bite();
+    println!("Eat a grape: {:?}", grapes);
+
     // Challenge: Uncomment the code below. Create a generic `bunny_nibbles`
     // function that:
     // - takes a mutable reference to any type that implements Bite
@@ -43,6 +65,14 @@ fn main() {
     //
     //bunny_nibbles(&mut carrot);
     //println!("Bunny nibbles for awhile: {:?}", carrot);
+
+    // SOLUTION:
+    bunny_nibbles(&mut carrot);
+    println!("Bunny nibbles for awhile: {:?}", carrot);
+
+    fn bunny_nibbles<T: Bite>(item: &mut T) {
+        for _ in 0..10 { item.bite()}
+    }
 }
 
 #[derive(Debug)] // This enables using the debugging format string "{:?}"
